a36d20f066b9d2fb47a70a26141a589cb72f46a0
GeleArthur <geleminecrafter@gmail.com>
2025-02-22 00:01:52 +0100

    Basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,61 @@
-﻿#define GLFW_INCLUDE_VULKAN
+﻿#include <exception>
+#include <iostream>
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+class HelloTraingle
+{
+public:
+    void run()
+    {
+        init_vulkan();
+        main_loop();
+        clean_up();
+    }
 
-#include <iostream>
+private:
+    void init_vulkan()
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void main_loop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void clean_up()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    GLFWwindow* window{};
+
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+};
 
 int main()
 {
-    glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while (!glfwWindowShouldClose(window))
+    HelloTraingle app;
+    try
     {
-        glfwPollEvents();
+        app.run();
     }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
-    return 0;
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
