7c63672f8cda40732c3a38626c2645db37ac810c
GeleArthur <geleminecrafter@gmail.com>
2025-05-16 16:42:21 +0200

    Added layout array

--- src/DescriptorSets/DescriptorLayoutBuilder.cpp
+++ src/DescriptorSets/DescriptorLayoutBuilder.cpp
@@ -12,17 +12,33 @@ namespace pvp
         , m_context(context)
     {
     }
-    DescriptorLayoutBuilder& DescriptorLayoutBuilder::add_binding(VkDescriptorType type, VkShaderStageFlags stage)
+    DescriptorLayoutBuilder& DescriptorLayoutBuilder::add_binding(VkDescriptorType type, VkShaderStageFlags stage, uint32_t amount)
     {
-        m_bindings.push_back(VkDescriptorSetLayoutBinding{ static_cast<uint32_t>(m_bindings.size()), type, 1u, stage, VK_NULL_HANDLE });
+        m_bindings.emplace_back(
+            static_cast<uint32_t>(m_bindings.size()),
+            type,
+            amount,
+            stage,
+            VK_NULL_HANDLE);
+        return *this;
+    }
+    DescriptorLayoutBuilder& DescriptorLayoutBuilder::add_flag(VkDescriptorBindingFlags flag)
+    {
+        m_flags.push_back(flag);
         return *this;
     }
 
     DescriptorCreator& DescriptorLayoutBuilder::build(uint32_t index)
     {
+        VkDescriptorSetLayoutBindingFlagsCreateInfo extra_flags{
+            .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_BINDING_FLAGS_CREATE_INFO,
+            .bindingCount = static_cast<uint32_t>(m_flags.size()),
+            .pBindingFlags = m_flags.data()
+        };
+
         const VkDescriptorSetLayoutCreateInfo layout_info{
             .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
-            .pNext = nullptr,
+            .pNext = &extra_flags,
             .flags = 0,
             .bindingCount = static_cast<uint32_t>(m_bindings.size()),
             .pBindings = m_bindings.data()
--- src/DescriptorSets/DescriptorLayoutBuilder.h
+++ src/DescriptorSets/DescriptorLayoutBuilder.h
@@ -9,13 +9,15 @@ namespace pvp
     class DescriptorLayoutBuilder
     {
     public:
-        DescriptorLayoutBuilder& add_binding(VkDescriptorType type, VkShaderStageFlags stage);
+        DescriptorLayoutBuilder& add_binding(VkDescriptorType type, VkShaderStageFlags stage, uint32_t amount = 1u);
+        DescriptorLayoutBuilder& add_flag(VkDescriptorBindingFlags flag);
         DescriptorCreator&       build(uint32_t index);
 
     private:
         friend class DescriptorCreator;
         explicit DescriptorLayoutBuilder(const Context& context, DescriptorCreator& creator);
         std::vector<VkDescriptorSetLayoutBinding> m_bindings;
+        std::vector<VkDescriptorBindingFlags>     m_flags;
         DescriptorCreator&                        m_creator;
         const Context&                            m_context;
     };
